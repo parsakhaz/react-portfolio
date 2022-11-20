39b823e2667655e22fed5cdadeb0a22dde654f31
parsasstudio <parsak@uw.edu>
2022-11-20 15:15:18 -0800

    complete UI redesign and redo with proper react components + readme pages

--- src/404.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Fourohfour() {
-    return (
-        <div className='h-screen leading-3'>
-            404
-        </div>
-    )
-}
---
+++ src/app/pages/links.tsx
@@ -0,0 +1,33 @@
+import {
+  FaStickyNote,
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+} from "react-icons/fa";
+
+export const links = [
+  {
+    index: 0,
+    title: "Find me on Github",
+    href: "https://github.com/parsakhaz/",
+    icon: <FaGithub />,
+  },
+  {
+    index: 1,
+    title: "Find me on LinkedIn",
+    href: "https://www.linkedin.com/in/parsas/",
+    icon: <FaLinkedin />,
+  },
+  {
+    index: 2,
+    title: "Contact me via email",
+    href: "mailto:parsak@uw.edu",
+    icon: <FaEnvelope />,
+  },
+  {
+    index: 3,
+    title: "Find me on Notion",
+    href: "https://parsas.notion.site/Home-74304ae8d3b7484c8b64b18b5b4ad427",
+    icon: <FaStickyNote />,
+  },
+];
